04487c6070ca59aefcb37bef6a84b27b7416f323
Yunnashi <yuridora3@gmail.com>
2023-11-12 18:40:26 +0900

    create RUST product ver

--- index.js
+++ index.js
@@ -12,30 +12,6 @@ app.use(methodOverride("_method"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// // 今回はDEMOのため、初期データをここで宣言する
-// let comments = [
-//   {
-//     id: uuid(),
-//     username: "Tarou",
-//     comment: "Happy Day!!!!!!",
-//   },
-//   {
-//     id: uuid(),
-//     username: "Yuki",
-//     comment: "I like chicken.",
-//   },
-//   {
-//     id: uuid(),
-//     username: "Risa",
-//     comment: "hooooooooooo!!",
-//   },
-//   {
-//     id: uuid(),
-//     username: "cat",
-//     comment: "MeowMeow",
-//   },
-// ];
-
 // Connect to MongoDB
 mongoose
   .connect("mongodb://mongo:27017/docker-node-mongo", {
@@ -45,83 +21,53 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-//const Item = require("./models/Item");
+const Product = require("./models/product");
+const categories = ["果物", "野菜", "乳製品"];
 
-//create Schema
-const movieSchema = new mongoose.Schema({
-  title: String,
-  year: Number,
-  score: Number,
-  rating: String,
+app.get("/products", async (req, res) => {
+  const products = await Product.find().catch((e) =>
+    res.status(404).json({ msg: "No items found" })
+  );
+  res.render("products/index", { products });
 });
 
-//create model ※modelのMovieというクラスが出来上がる
-const Movie = mongoose.model("Movie", movieSchema);
-
-//modelのインスタンス化
-const amadeus = new Movie({
-  title: "Amadeus",
-  year: 1986,
-  score: 9.2,
-  rating: "R",
+app.get("/products/new", (req, res) => {
+  res.render("products/new", { categories });
 });
 
-console.log("amadeus→→", amadeus);
-amadeus.save();
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  console.log(newProduct);
+  res.redirect(`/products/${newProduct._id}`);
+});
 
-// app.get("/", (req, res) => {
-//   Item.find()
-//     .then((items) => res.render("index", { items }))
-//     .catch((err) => res.status(404).json({ msg: "No items found" }));
-// });
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/show", { product });
+});
 
-// app.post("/item/add", (req, res) => {
-//   const newItem = new Item({
-//     name: req.body.name,
-//   });
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
 
-//   newItem.save().then((item) => res.redirect("/"));
-// });
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
+});
 
-// app.get("/comments", (req, res) => {
-//   res.render("comments/index", { comments });
-// });
-
-// app.get("/comments/new", (req, res) => {
-//   res.render("comments/new");
-// });
-
-// app.post("/comments", (req, res) => {
-//   const { username, comment } = req.body;
-//   comments.push({ username, comment, id: uuid() });
-//   res.redirect("/comments");
-// });
-
-// app.get("/comments/:id", (req, res) => {
-//   const { id } = req.params;
-//   const comment = comments.find((c) => c.id === id);
-//   res.render("comments/show", { comment });
-// });
-
-// app.get("/comments/:id/edit", (req, res) => {
-//   const { id } = req.params;
-//   const comment = comments.find((c) => c.id === id);
-//   res.render("comments/edit", { comment });
-// });
-
-// app.patch("/comments/:id", (req, res) => {
-//   const { id } = req.params;
-//   const newCommentText = req.body.comment;
-//   const foundComment = comments.find((c) => c.id === id);
-//   foundComment.comment = newCommentText;
-//   res.redirect("/comments");
-// });
-
-// app.delete("/comments/:id", (req, res) => {
-//   const { id } = req.params;
-//   comments = comments.filter((c) => c.id !== id);
-//   res.redirect("/comments");
-// });
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
 
 app.get("*", (req, res) => {
   res.send("404 Not found!");
